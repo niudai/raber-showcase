22f6bc35dbd9e3de9deaf652fd276101196e7455
niudai <niudai.geek@bytedance.com>
2023-04-09 11:31:12 +0800

    fix

--- apps/next-18-nextjs/pages/nextjs.tsx
+++ apps/next-18-nextjs/pages/nextjs.tsx
@@ -1,5 +1,5 @@
 import { Canvas, getPage } from "@raber/react";
-import { NextJSCard } from "../components/eth/nextjs-card";
+import "../components/eth/nextjs-card";
 
 export async function getStaticProps() {
 	const data = await getPage('nextjs');
@@ -10,7 +10,6 @@ export default function NextJS({ data }: any) {
 	return (
 		<main>
 			<h1>Welcome to Next.js</h1>
-			<NextJSCard vKey="nextjs"/>
 			<Canvas data={data} id="nextjs"></Canvas>
 		</main>
 	)
